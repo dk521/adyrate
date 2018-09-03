77693b2213d44ba745c4a8e71521fc6b38675e05
SILU-007 <sunindus.sk@gmail.com>
2018-09-03 10:20:00 +0530

    starting location tracker

---
+++ app/src/main/java/com/gluespark/joker/gluespark/Activities/SearchActivity.java
@@ -0,0 +1,17 @@
+package com.gluespark.joker.gluespark.Activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.gluespark.joker.gluespark.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+    }
+}
